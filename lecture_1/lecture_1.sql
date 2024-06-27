22be2426865f87657180d4c191ec7695cb25478e
jagdish-18 <shiyaljagdish18@gmail.com>
2024-06-27 11:17:25 +0530

    27-06-2024

---
+++ lecture_1/lecture_1.sql
@@ -0,0 +1,28 @@
+=> connect mysql
+--> \sql --first command
+-->\connect root@localhost:3306 --> connect sql;
+
+=> show all databsae command
+-->show databases;
+
+=> create database command;
+--> create database <databasename>;
+
+=> use perticular database command;
+--> use <databasename>;
+
+=> show perticular database tables;
+--> show tables;
+
+=> create new table
+--> create table user( 
+-- fisrtname varchar(50),
+-- lastname varchar(50),
+-- email varcahr(50),
+-- gender varchar(10),
+-- mobileNo int
+--);
+
+=> describe or show perticular table (row and column)
+--> desc user;
+--> describe user;
